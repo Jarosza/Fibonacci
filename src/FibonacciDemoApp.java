7048b4bd89b42abf22b90391a2c6f4737bec80ba
Anna Jarosz <annajarosz1310@gmail.com>
2022-05-05 16:49:48 +0200

    I introduced "BigInteger" and the variable that holds the results.

--- src/FibonacciDemoApp.java
+++ src/FibonacciDemoApp.java
@@ -1,23 +1,34 @@
 import java.math.BigInteger;
+import java.util.HashMap;
+import java.util.Map;
 
 public class FibonacciDemoApp {
 
 
     public static void main(String[] args) {
-        for (int i = 1; i < 50; i++){
+        for (int i = 1; i <= 50; i++){
             System.out.println(i + " ==>" + fibo(i));
         }
     }
 
-        static BigInteger fibo (int n){
+        static Map<Integer, BigInteger> results = new HashMap<>();
+
+        static BigInteger fibo(int n){
             if (n == 0) {
                 return BigInteger.ZERO;
             }
-            if (n == 1) {
+            if(n == 1) {
                 return BigInteger.ONE;
             }
+            if (results.containsKey(n)){
+                return results.get(n);
+            }
 
-            return fibo(n - 1).add(fibo(n - 2));
+
+            BigInteger result = fibo(n-1).add(fibo(n - 2));
+            results.put(n, result);
+
+            return result;
 
         }
 }
