5f2cf08e34c101b4f955cd08d7be773644fabc94
Anna Jarosz <annajarosz1310@gmail.com>
2022-05-17 10:04:37 +0200

    Simple Fibonacci sequence. #Recursive implementation.

--- src/FibonacciDemoApp.java
+++ src/FibonacciDemoApp.java
@@ -1,34 +1,33 @@
 import java.math.BigInteger;
+import java.sql.SQLOutput;
 import java.util.HashMap;
 import java.util.Map;
+import java.util.Scanner;
 
 public class FibonacciDemoApp {
-
-
+    static Map<Integer, BigInteger> results = new HashMap<>();
     public static void main(String[] args) {
-        for (int i = 1; i <= 50; i++){
-            System.out.println(i + " ==>" + fibo(i));
-        }
+        System.out.println("n");
+        Scanner scan = new Scanner(System.in);
+        int n = scan.nextInt();
+        BigInteger result = fibo(n-1).add(fibo(n - 2));
+        results.put(n, result);
+
+        System.out.println(result);
     }
+        static BigInteger fibo(int n) {
+         if (n == 0) {
+            return BigInteger.ZERO;
+         }
+         if (n == 1) {
+            return BigInteger.ONE;
+         }
+         if(results.containsKey(n)){
+             return results.get(n);
+         }
+         BigInteger result = fibo(n - 1).add(fibo(n - 2));
+         results.put(n, result);
 
-        static Map<Integer, BigInteger> results = new HashMap<>();
-
-        static BigInteger fibo(int n){
-            if (n == 0) {
-                return BigInteger.ZERO;
-            }
-            if(n == 1) {
-                return BigInteger.ONE;
-            }
-            if (results.containsKey(n)){
-                return results.get(n);
-            }
-
-
-            BigInteger result = fibo(n-1).add(fibo(n - 2));
-            results.put(n, result);
-
-            return result;
-
-        }
+            return fibo(n - 1).add(fibo(n - 2));
+    }
 }
